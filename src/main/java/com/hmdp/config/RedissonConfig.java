aee32b26e2de8e3b0908cf902c2a9cfb5f544273
yangjiao18 <1277651608qq.com>
2022-03-10 18:15:28 +0800

    test

---
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -0,0 +1,20 @@
+package com.hmdp.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient redissonClient(){
+        // 配置
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://120.24.170.218:6379").setPassword("1993");
+        // 创建RedissonClient对象
+        return Redisson.create(config);
+    }
+}
---
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -0,0 +1,19 @@
+package com.hmdp.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 虎哥
+ */
+@RestController
+@RequestMapping("/follow")
+public class FollowController {
+
+}
